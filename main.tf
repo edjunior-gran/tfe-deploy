10c4fb9d13a4dd3c3d9ed6d84868f468508fdfce
edson.junior <edson.junior@gran.com>
2025-01-20 16:55:21 -0300

    feature: module tf  for deploy

--- main.tf
+++ main.tf
@@ -11,6 +11,48 @@ locals {
     }
 }
 
+
+
+
+###############################
+# create ecr repository
+###############################
+resource "aws_ecr_repository" "ecr" {
+  name = "${local.name}-repo"
+  image_tag_mutability = "MUTABLE"
+  force_delete = true
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = local.tags
+}
+
+#################################
+# create lifecycle policy ecr repository
+#################################
+
+resource "aws_ecr_lifecycle_policy" "lifecycle_policy" {
+  repository = aws_ecr_repository.ecr.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 6 images",
+        selection    = {
+          tagStatus    = "any",
+          countType    = "imageCountMoreThan",
+          countNumber  = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+
 #################################
 # Build Image
 #################################
